24cbec1dc18b7def657cdf454f7ccfd055910553
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2019-10-05 15:41:22 +0530

    Add login page

---
+++ README.md
@@ -0,0 +1,5 @@
+# Syringe
+
+Biochemistry Blood Test: iOS Project 2019
+
+Link to download Assets :  https://drive.google.com/open?id=1PbfbH2Fe1NSaYQTJmmmt_JJgOHhBxlRD
--- Syringe/ViewController.swift
+++ Syringe/ViewController.swift
@@ -2,19 +2,34 @@
 //  ViewController.swift
 //  Syringe
 //
-//  Created by student on 01/10/19.
+//  Created by Sarathkrishnan Ramesh on 05/10/19.
 //  Copyright © 2019 student. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UITextField {
+    func setBorder() {
+        self.borderStyle = .none
+        self.layer.backgroundColor = UIColor.white.cgColor
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var usernameEditField: UITextField!
+    @IBOutlet weak var passwordEditField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        usernameEditField.setBorder()
+        passwordEditField.setBorder()
+    }
 
 }
 
