984fff5072d04f271a6b158a2dfe4fac0817fd77
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2019-10-08 16:04:23 +0530

    Update Registration Page

--- Syringe/RegisterViewController.swift
+++ Syringe/RegisterViewController.swift
@@ -2,18 +2,40 @@
 //  RegisterViewController.swift
 //  Syringe
 //
-//  Created by student on 05/10/19.
+//  Created by Sarathkrishnan Ramesh on 05/10/19.
 //  Copyright © 2019 student. All rights reserved.
 //
 
 import UIKit
 import Firebase
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    // Number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return bloodGroupPickerData.count
+    }
+    
+    // The data to return fopr the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return bloodGroupPickerData[row]
+    }
+    
 
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var mobileNumberTextField: UITextField!
+    @IBOutlet weak var bloodGroupPicker: UIPickerView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    var bloodGroupPickerData: [String] = [String]()
     
     @IBAction func registerButtonPressed(_ sender: Any) {
         if(confirmPasswordTextField.text == passwordTextField.text) {
@@ -28,10 +50,21 @@ class RegisterViewController: UIViewController {
             }
         }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Adding custom border to text fields
+        passwordTextField.setBorder()
+        confirmPasswordTextField.setBorder()
+        emailTextField.setBorder()
+        nameTextField.setBorder()
+        mobileNumberTextField.setBorder()
+        
+        // Initializing the blood group picker
+        bloodGroupPicker.delegate = self
+        bloodGroupPicker.dataSource = self
+        bloodGroupPickerData = ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"];
     }
 
     /*
