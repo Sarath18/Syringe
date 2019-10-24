cd8995f905a97f247ce7663bb18279191f47b36e
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2019-10-24 18:48:51 +0530

    Add labtech dashboard
    Show list of all the patients in the dashboard

--- Syringe/LabTechnicialViewController.swift
+++ Syringe/LabTechnicialViewController.swift
@@ -7,25 +7,74 @@
 //
 
 import UIKit
+import Firebase
 
-class LabTechnicialViewController: UIViewController {
+class LabTechnicialViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var appointments: [String] = []
+    var refreshControl = UIRefreshControl()
+    
+    let cellReuseIdentifier = "cell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.appointments.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = (self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.appointments[indexPath.row]
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+    
+    func fetchAppointmentDetails(mode : Int) {
+        if(mode == 1) {
+            self.appointments = [];
+        }
+        let appointmentDB = Database.database().reference(withPath: "appointments/1");
+        
+        appointmentDB.observeSingleEvent(of: .value, with: { (snapshot) in
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                self.appointments.append(snap.childSnapshot(forPath: "full_name").value as! String);
+            }
+            self.tableView.reloadData()
+            self.refreshControl.endRefreshing()
+        })
+    }
+
     //var userId: String?
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true;
-        //print("LAB TECH VIEW CONTROLLER: " + userId!)
-        // Do any additional setup after loading the view.
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        self.tableView.reloadData()
+        fetchAppointmentDetails(mode: 0);
+        
+        tableView.addSubview(refreshControl)
+        refreshControl.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func refresh(_ sender: Any) {
+        fetchAppointmentDetails(mode: 1)
     }
-    */
-
+    
 }
